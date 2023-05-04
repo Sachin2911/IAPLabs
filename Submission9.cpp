5121fc0188243843454e7fc2ba2952281fe449c9
Sach2911 <Sachinmohan29@gmail.com>
2023-05-04 14:33:04 +0000

    lab9 start

---
+++ Submission9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string userinp;
+    int index = 0;
+    string vowel_counts[10];
+    do{
+        if (userinp != "end"){
+        int tmp_vowel_count = 0;
+        cin >> userinp;
+
+        for(int i = 0; i<userinp.length(); i++){
+            
+        }
+
+        cout << tmp_vowel_count;
+        }
+    }while(userinp != "end");
+}
